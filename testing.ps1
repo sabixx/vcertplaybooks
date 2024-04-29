c18cab0de6f56e8fcc78b86c92fdd165748d5692
Jens Sabitzer <jens.sabitzer@venafi.com>
2024-04-29 14:57:41 +0200

    done3

--- testing.ps1
+++ testing.ps1
@@ -7,7 +7,7 @@ $tempPath = [System.IO.Path]::GetTempPath()
 $logFilePath = Join-Path -Path  "$tempPath" "vcert_testing_log.txt"
 
 # Function to append log messages with timestamps
-<#
+
 function Log-Message {
     param (
         [string]$Message
@@ -16,22 +16,22 @@ function Log-Message {
     Add-Content -Path $logFilePath -Value "[$timestamp] $Message"
     Write-Host $Message
 } 
-#>
+
 
 Write-Host "==== Start ===="
-#Log-Message "==== Start ===="
+Log-Message "==== Start ===="
 
 
 # check is playbook_url was provided
 if (-not $playbook_url) {
-    #Log-Message "no playbook_url provided, existing."
+    Log-Message "no playbook_url provided, existing."
     Write-Host "no playbook_url provided, existing."
     
     exit
 } else {
-    #Log-Message "using playbook_url = $playbook_url"
+    Log-Message "using playbook_url = $playbook_url"
     Write-Host  "using playbook_url = $playbook_url"
 }
 
-#Log-Message "still running.... "
+Log-Message "still running.... "
 Write-Host  "1still running.... "
